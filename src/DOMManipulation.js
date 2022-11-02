61a2ec074e74ed0d211958bec8a27f9885e3de4c
Steph <stazatorres@hotmail.com>
2022-11-02 17:58:44 +0000

    hover effect added to small icons, basic form and listener added to add projects

--- src/DOMManipulation.js
+++ src/DOMManipulation.js
@@ -1,74 +1,14 @@
 import { Project, Task, projectList} from './classes'
 import './style.css';
 
-/* 
-const content = document.getElementById('content')
-const projectFormModal = document.createElement('dialog');
-content.appendChild(projectFormModal);
- */
+const addProjectButton = document.getElementById('addProject');
+const addProjectModal = document.getElementById('projectFormModal');
 
 
-/* onLoad to creat DOM might not work as will want to access a lot of the different elements 
-    from different functions, what about using IIFE */
-/* export function onLoad(){
+addProjectButton.addEventListener('click', addProject);
 
+function addProject(){
 
-    displayProjectForm()
-
-
-    const addProjectButton = document.createElement('button');
-    addProjectButton.innerText = "Create Project";
-    content.appendChild(addProjectButton);
-    
-    addProjectButton.addEventListener('click', () => {
-        projectFormModal.showModal();
-    });
-
-
-}; 
-
-function displayProjectForm(){
-
-    const projectForm = document.createElement('form');
-    const formLegend = document.createElement('legend');
-    const projectName = document.createElement('input');
-    const projectDesscription = document.createElement('input');
-    const projectSumbitButton = document.createElement('button');
-    const resetFromButton = document.createElement('button');
-    const cancelProjectButton = document.createElement('button');
-
-
-    projectFormModal.appendChild(projectForm);
-    projectFormModal.className = "projectFormModal";
-    projectForm.appendChild(formLegend);
-    formLegend.innerText = 'New Project';
-
-    projectForm.appendChild(projectName);
-    projectName.setAttribute('type', 'text');
-    projectName.setAttribute('name', 'name');
-    projectName.setAttribute('placeholder', 'Project Name');
-
-    projectForm.appendChild(projectDesscription);
-    projectDesscription.setAttribute('type', 'text');
-    projectDesscription.setAttribute('name', 'productDescription');
-    projectDesscription.setAttribute('placeholder', 'Project Description');
-
-
-
-    projectForm.appendChild(projectSumbitButton);
-    projectSumbitButton.innerText = 'Add Project';
-        
-    projectForm.appendChild(resetFromButton);
-    resetFromButton.innerText = 'Reset';
-
-    projectForm.appendChild(cancelProjectButton);
-    cancelProjectButton.innerText = '&times';
+    addProjectModal.showModal();
 
 }
-
-
-function displayProjectArray(){
-
-    projectList.forEach()
-
-} */
